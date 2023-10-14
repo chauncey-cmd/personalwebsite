6348db0fa43ddb7bc13200adc20fcbf0344f71de
Vinnie Angellotti <vinnieangellotti@Belles-MacBook-Pro.local>
2023-10-13 23:55:47 -0700

    personalization

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Vinnie's personal website. This is hosted on Azure and will be updated soon.
         </p>
         <a
           className="App-link"
