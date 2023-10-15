c1b89ad114f3a6371b7346d5f2e517c6192894a6
Vinnie Angellotti <vinnieangellotti@Belles-MacBook-Pro.local>
2023-10-14 21:10:24 -0700

    adding styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>About Me</h1>
+    </header>
+  );
+}
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I am Vinnie, and I am a Program Manager. This is my personal website where I share information about myself, my interests, and my experiences.
+      </p>
+      <p>
+        I am from Wisconsin and I work at Microsoft.
+      </p>
+    </section>
+  );
+}
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>
+        If you'd like to get in touch with me, you can reach me at vinnie@gmail.com. Feel free to drop me a message, and I'll get back to you as soon as possible.
+      </p>
+      <p>
+        You can also find me on social media: vinnie.angel.
+      </p>
+    </section>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is Vinnie's personal website. This is hosted on Azure and will be updated soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <About />
+        <Contact />
+      </main>
     </div>
   );
 }
