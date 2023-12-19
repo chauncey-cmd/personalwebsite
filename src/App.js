74ce90f09b63b75aa3a78a04eecd4af040147fe3
Vinnie Angellotti <77800319+vinnieangel@users.noreply.github.com>
2023-12-19 09:51:34 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function About() {
     <section id="about">
       <h2>About Me</h2>
       <p>
-        Hello! I am Vinnie, and I am a Program Manager. This is my personal website where I share information about myself, my interests, and my experiences.
+        Hello! I am Vinnies, and I am a Program Manager. This is my personal website where I share information about myself, my interests, and my experiences.
       </p>
       <p>
         I am from Wisconsin and I work at Microsoft.
